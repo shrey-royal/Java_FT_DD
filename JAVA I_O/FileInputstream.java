55e92096190b3ed7704fc1e8a7a473ad3b428713
Shrey K <srkkadia@gmail.com>
2023-07-10 17:04:21 +0530

    Task - fout/fin

--- JAVA I_O/FileInputstream.java
+++ JAVA I_O/FileInputstream.java
@@ -5,11 +5,26 @@ public class FileInputstream {
     public static void main(String[] args) {
         try {
             FileInputStream fin = new FileInputStream("Output_Text_Files\\fout.txt");
-            int i = fin.read();
-            System.out.println((char) i);
+            // int i = fin.read();
+            // System.out.println((char) i);
+
+            //scan all the characters
+            int i = 0;
+            while ((i = fin.read()) != -1) {
+                System.out.print((char) i);
+            }
+
+
             fin.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
+
+/*
+ * Write 5 lines about the difference between FileInputStream and FileOutputStream.
+ * > add that lines into a file called "FileInputstream.txt" and save it.
+ * >  then use FileInputStream to read the file and print the lines.
+ */
